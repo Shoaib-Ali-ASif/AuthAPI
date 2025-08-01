4379090a10e9d06e8ecde5604e40a0613841d0fb
Shoaib Ali Asif <eagleshebi@gmail.com>
2025-08-01 23:55:01 +0500

    Initial commit

---
+++ Controllers/UserAuthController.cs
@@ -0,0 +1,123 @@
+﻿using AuthAPI.Data;
+using AuthAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.V4.Pages.Account.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+using LoginModel = AuthAPI.Models.LoginModel;
+using RegisterModel = AuthAPI.Models.RegisterModel;
+
+namespace AuthAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserAuthController : ControllerBase
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+        private readonly string? _jwtKey;
+        private readonly string? _JwtIssuer;
+        private readonly string? _JwtAudience;
+        private readonly int _JwtExpiry;
+
+        public UserAuthController(
+      UserManager<ApplicationUser> userManager,
+      SignInManager<ApplicationUser> signInManager,
+      IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _jwtKey = configuration["Jwt:Key"];
+            _JwtIssuer = configuration["Jwt:Issuer"];
+            _JwtAudience = configuration["Jwt:Audience"];
+
+            // Safe parsing
+            if (!int.TryParse(configuration["Jwt:ExpiryMinutes"], out _JwtExpiry))
+            {
+                _JwtExpiry = 60; // default to 60 if not found
+            }
+        }
+
+
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterModel registerModel)
+        {
+            if (registerModel == null
+                || string.IsNullOrEmpty(registerModel.Name)
+                || string.IsNullOrEmpty(registerModel.Email)
+                || string.IsNullOrEmpty(registerModel.Password)
+                )
+            {
+                return BadRequest("Invalid registration data.");
+            }
+
+            var existingUser = await _userManager.FindByEmailAsync(registerModel.Email);
+            if (existingUser != null)
+            {
+                return Conflict("User with this email already exists.");
+
+            }
+            var user = new ApplicationUser
+            {
+                UserName = registerModel.Email,
+                Email = registerModel.Email,
+                Name = registerModel.Name
+            };
+            var result = await _userManager.CreateAsync(user, registerModel.Password);
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+            return Ok("User Created Succesfully");
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel loginModel)
+        {
+            var user = await _userManager.FindByEmailAsync(loginModel.Email);
+            if (user == null)
+            {
+                return Unauthorized(new {  success = false, message = "Invalid Username or Password"});
+            }
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginModel.Password ,false);
+            if (!result.Succeeded)
+            {
+                return Unauthorized(new { success = false, message = "Invalid email or password." });
+            }
+            var token = GenerateJWTToken(user);
+            return Ok(new { success = true, token = token, message = "Login Successful" });
+
+        }
+
+        [HttpPost("Logout")]
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return Ok(new { success = true, message = "Logout Successful" });
+        }
+        private string GenerateJWTToken(ApplicationUser user) { 
+            var Claims = new []
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim("Name", user.Name),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new System.IdentityModel.Tokens.Jwt.JwtSecurityToken(
+                claims: Claims,
+                expires: DateTime.Now.AddMinutes(_JwtExpiry),
+                signingCredentials: creds
+            );
+            return new System.IdentityModel.Tokens.Jwt.JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,75 @@
+using AuthAPI.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+// Configure Identity
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 1;
+    options.Password.RequiredUniqueChars = 0;
+
+
+}).AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+//JWT configuration
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = "myIssuer",
+        ValidAudience = "myAudience",
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes("MySuperSecretKey123456")
+        )
+    };
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "AtuhAPI");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
